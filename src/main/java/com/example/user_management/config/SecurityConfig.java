458c330e9668de2a236d21f85fea145d7a0de15e
hoquanglanh <lanhvivu1245@gmail.com>
2025-08-24 13:39:26 +0700

    add register

--- src/main/java/com/example/user_management/config/SecurityConfig.java
+++ src/main/java/com/example/user_management/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/css/**", "/js/**").permitAll() // cho phép login page & static
+                        .requestMatchers("/login", "/register", "/css/**", "/js/**").permitAll() // cho phép login page & static
                         .requestMatchers("/h2-console/**").permitAll()              // ✅ Cho phép H2 Console
                         .requestMatchers("/users/**").hasRole("ADMIN")              // chỉ ADMIN truy cập users
                         .anyRequest().authenticated()
